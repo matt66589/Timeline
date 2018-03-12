327d28ac3c2f485e984ca470b1d766c7d235a81b
Matthew Peterson <matthewp888@gmail.com>
2018-03-11 21:59:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+(function(){
+	$(document).ready(main);
+
+	function main()
+	{
+		var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+		//example http://visjs.org/examples/timeline/groups/groupsOrdering.html
+	}
+})();
